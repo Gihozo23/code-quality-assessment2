286d3b7bfc1995e6cf2d3598bbc873622cb2585d
Gihozo23 <christellegihozo23@gmail.com>
2024-08-22 09:43:59 +0200

    chore: refactor the calculateFactorial function implementation

--- calculateFactorial.js
+++ calculateFactorial.js
@@ -1,11 +1,5 @@
-function calculateFactorial(n) {
-    if (n === 0 || n === 1) {
-      return 1;
-    } else {
-      var factorial = 1;
-      for (var i = 2; i <= n; i++) {
-        factorial *= i;
-      }
-      return factorial;
-    }
-  }
+function calculateFactorial(number) {
+    if (number < 0) return"Please, use a positive number!";
+    if (number === 0 || number === 1) return 1;
+    return number * calculateFactorial(number - 1);
+}
